043c8f27336b51afdcb3f6c546703984279f0e8e
ChintaLokee <lokesh@artiumacademy.com>
2023-05-30 15:12:48 +0530

    optimized  modified the code

--- MyFirstAWSProgram.py
+++ MyFirstAWSProgram.py
@@ -2,19 +2,11 @@ import boto3
 import pandas as pd
 
 
-print(f"boto3 version is {boto3.__version__}")
-print(f"pandas version is {pd.__version__}")
-s3=boto3.resource("s3",aws_access_key_id="AKIAVQRYFTDWKYVT6DGL",aws_secret_access_key="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")
 
-for bucket in s3.buckets.all():
-    print(bucket.name)
 
-    
-def uploadFile(awsFilePath:str,filename:str):   
-    s3 = boto3.client("s3",aws_access_key_id="AKIAVQRYFTDWKYVT6DGL",aws_secret_access_key="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")
-    bucket_name="s301myawsbucket"
-    file_key=awsFilePath+'/Test.xlsx'
-    # filename="/Users/www.abcom.in/Downloads/AWSProject/Files/Time Slots from June 2023 (2).xlsx"
+def uploadFile(awsFilePath:str,filename:str,aws_access_key_id:str,aws_secret_access_key:str,
+            bucket_name:str,file_key:str):   
+    s3 = boto3.client("s3",aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
     result = s3.list_objects_v2(Bucket=bucket_name, Prefix=file_key)
     if 'Contents' in result:
         print("file exists in the bucket.")
@@ -39,4 +31,23 @@ print(f"cols is {cols}")
 for col in range(cols):
     print(f" first row {col+1 }  col is {df.iloc[0,col]}")
     print("---------")
-uploadFile(df.iloc[0,0]+"/"+df.iloc[0,0+1],df.iloc[0,0+2])
+
+
+aws_access_key_id="<your access key id>"
+aws_secret_access_key="<your aws secret access key>"  
+bucket_name="s301myawsbucket"
+awsFilePath=df.iloc[0,0]+"/"+df.iloc[0,0+1]
+file_key=awsFilePath+'/Test.xlsx'
+
+
+print(f"boto3 version is {boto3.__version__}")
+print(f"pandas version is {pd.__version__}")
+s3=boto3.resource("s3",aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
+
+
+# for bucket in s3.buckets.all():
+#     print(f"bucket name {bucket.name}")  
+
+
+
+uploadFile(awsFilePath,df.iloc[0,0+2],aws_access_key_id,aws_secret_access_key,bucket_name,file_key)
